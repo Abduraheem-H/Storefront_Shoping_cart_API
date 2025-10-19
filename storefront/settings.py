4e0f59b45cd5c53cdc9f1463b849c401687a9cf5
Abduraheem Hussein <abdurahim.muzemil@aait.edu.et>
2025-10-19 16:35:13 +0300

    Add custom user model and update related configurations for authentication

--- storefront/settings.py
+++ storefront/settings.py
@@ -50,9 +50,9 @@ INSTALLED_APPS = [
     "playground",
     "debug_toolbar",
     "store",
-    "store_custom",
     "tags",
     "likes",
+    "core",
 ]
 
 MIDDLEWARE = [
@@ -154,3 +154,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "COERCE_DECIMAL_TO_STRING": False,
 }
+
+AUTH_USER_MODEL = "core.User"
